8aa04621d962acca2a83a985db5d437ce6536dc9
罗展鹏 <zhl357@pitt.edu>
2025-09-15 10:48:00 -0400

    add one video havenot solved the problem yet

--- static/videos/convert.sh
+++ static/videos/convert.sh
@@ -1,28 +1,6 @@
-ffmpeg -i market_updated.mp4 \
+ffmpeg -i static/videos/4DGS_.mp4 \
   -c:v libx264 -profile:v main -pix_fmt yuv420p -crf 23 \
   -movflags +faststart \
-  -y market_fixed.mp4
+  -y 4DGS_eninstein.mp4
 
 
-ffmpeg -i ein_updated.mp4 \
-  -c:v libx264 -profile:v main -pix_fmt yuv420p -crf 23 \
-  -movflags +faststart \
-  -y ein_fixed.mp4
-
-
-ffmpeg -i panda_updated.mp4 \
-  -c:v libx264 -profile:v main -pix_fmt yuv420p -crf 23 \
-  -movflags +faststart \
-  -y panda_fixed.mp4
-
-
-ffmpeg -i pizza_updated.mp4 \
-  -c:v libx264 -profile:v main -pix_fmt yuv420p -crf 23 \
-  -movflags +faststart \
-  -y pizza_fixed.mp4
-
-
-ffmpeg -i sheep_updated.mp4 \
-  -c:v libx264 -profile:v main -pix_fmt yuv420p -crf 23 \
-  -movflags +faststart \
-  -y sheep_fixed.mp4
